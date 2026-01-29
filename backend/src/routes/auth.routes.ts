0512cf5458f33fdd1785d078f7225fa13180ad91
blissexodia <binnoley@gmail.com>
2026-01-30 00:52:14 +0545

    auth routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
 
-
 let authController = new AuthController();
 const router = Router();
 
-router.post("/register",authController.register);
-router.post("/login",authController.login);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+
+// New profile update route
+router.put("/update-profile", authController.updateProfile); // This will handle profile update
 
 export default router;
