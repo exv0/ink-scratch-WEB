d4d7b32b1064a3c3f6d207712edca4cf52680ff2
blissexodia <binnoley@gmail.com>
2026-02-03 19:08:04 +0545

    layout

---
+++ frontend/app/admin/layout.tsx
@@ -0,0 +1,6 @@
+// app/admin/layout.tsx
+import AdminProtectedRoute from "@/components/AdminProtectedRoute";
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return <AdminProtectedRoute>{children}</AdminProtectedRoute>;
+}
