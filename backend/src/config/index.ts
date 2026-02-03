04ce76e0ffaec40705b36e4eeed3d5f9df78eb4a
blissexodia <binnoley@gmail.com>
2026-02-03 20:17:30 +0545

    index

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const PORT: number = 
     process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const MONGODB_URI: string =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb';
+    process.env.MONGO_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb';
 
 export const JWT_SECRET: string =
     process.env.JWT_SECRET || 'default';
